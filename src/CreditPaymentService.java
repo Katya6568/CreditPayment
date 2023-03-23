b77b4e87e20c25e7f99e9f6dd8ce3c6d2e6cda29
Ekaterina Pavlova <sone4ka1414@gmail.com>
2023-03-23 17:41:02 +0300

    calculation correction

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,11 @@
 public class CreditPaymentService {
 
-    public int calculate(int sum, double rate, int months) {
+    public int calculate(int sum, double ratePercent, int months) {
         int payment;
 
-        double monthRate = rate / months;
-        payment = (int) ((int) sum * (monthRate * Math.pow (1 + monthRate, months)) / Math.pow (1 + monthRate, months) - 1);
+        double rate = ratePercent / 100;
+        double monthRate = rate / 12;
+        payment = (int) (sum * ((monthRate * Math.pow (1 + monthRate, months))) / (Math.pow (1 + monthRate, months) - 1));
 
         return payment;
     }
